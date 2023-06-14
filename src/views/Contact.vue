4080aba2375265e7cb8353647ce8d777d4f02c4d
ign1ka <85178064+ign1ka@users.noreply.github.com>
2023-06-14 11:24:57 -0400

    updated contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,26 +3,26 @@
     
     <h1>Let's get in touch</h1>
 
-    <div style="margin-bottom:40px;">I'm looking for a job !<br/>Feel free to contact me about any opportunity, or just to chat about cooking, kittens or anything else.</div>
+    <div style="margin-bottom:40px;">I'm looking for a job!<br/>Feel free to contact me about any opportunity, or just to chat about games, music or anything else.</div>
 
     <ul>
-      <li>
+      <!--<li>
         <i class="fa fa-user-circle-o fa-lg fa-fw"></i>
         <a href="http://mywebsite.com" target="_blank">https://mywebsite.com</a>
-      </li>
+      </li>-->
       <li>
         <i class="fa fa-envelope-o fa-lg fa-fw"></i>
-        <a href="mailto:my@email.com" target="_blank">my@email.com</a>
+        <a href="mailto:nilesdorn@gmail.com" target="_blank">nilesdorn@gmail.com</a>
       </li>
       <li>
         <i class="fa fa-linkedin fa-lg fa-fw" style="vertical-align: -5%;"></i>
-        <a href="https://www.linkedin.com/me" target="_blank">linkedin.com/me</a>
+        <a href="https://www.linkedin.com/in/nilesdorn" target="_blank">linkedin.com/in/nilesdorn</a>
       </li>
       <li>
         <i class="fa fa-github fa-lg fa-fw"></i>
-        <a href="https://github.com/me" target="_blank">github.com/me</a>
+        <a href="https://github.com/nilesdorn" target="_blank">github.com/nilesdorn</a>
       </li>
-      <li>
+      <!--<li>
         <i class="fa fa-gamepad fa-lg fa-fw"></i>
         <a href="https://myself.itch.io" target="_blank">myself.itch.io</a>
       </li>
@@ -33,7 +33,7 @@
       <li>
         <i class="fa fa-steam fa-lg fa-fw"></i>
         <a href="https://steamcommunity.com/id/me" target="_blank">steam.com/me</a>
-      </li>
+      </li>-->
     </ul>
 
   </div>
