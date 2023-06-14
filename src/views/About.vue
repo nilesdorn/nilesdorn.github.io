6314acc2af42b7660821591c577b512ecf7834f9
ign1ka <85178064+ign1ka@users.noreply.github.com>
2023-06-14 10:19:15 -0400

    updated 'about me' page and website title/description

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,17 +4,18 @@
 
     <div class="paragraph">
       <div>
-        I'm <strong>John Matrix</strong>, a former Delta Force operative, now a hobbyist lumberjack.<br/>
-        I spend my days living alone with my daughter Jenny, and cutting trees and carrying trunks around to keep in shape.
+        I'm <strong>Niles Dorn</strong>, a versatile software developer and hobbyist musician.<br/>
+        
+        I have strong skils across many disciplines, including object-oriented programming, the Unity and Unreal game engines, XR development, and audio programming.
       </div>
 
-      <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">stuff</router-link>, on <router-link to="/other-projects">other stuff</router-link>, and took part in <router-link to="/resume">a few things</router-link> as well.</div>
+      <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">development projects</router-link>, released <router-link to="/other-projects">independent music</router-link>, and picked up skills in <router-link to="/resume">other fields</router-link> as well.</div>
 
-      <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> as a monk, like my good friend John Rambo did a few years back. You can reach me at <a href="mailto:johnmatrix@deltaforce.us">johnmatrix@deltaforce.us</a> or <router-link to="/contact">through here</router-link>.</div>
+      <div style="margin-top: 40px;">I'm <strong>currently looking for full-time employment</strong>. You can reach me at <a href="mailto:nilesdorn@gmail.com">nilesdorn@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
     </div>
 
     <div class="photo">
-      <img src="img/avatar.png" alt="Avatar of John" /> 
+      <img src="img/avatar.png" alt="Avatar of Niles" /> 
     </div>
 
     <div style="clear:both"></div>
