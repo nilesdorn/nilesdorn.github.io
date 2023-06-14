bac164d2ed7be5c866070c243fc90cc275d8199a
ign1ka <85178064+ign1ka@users.noreply.github.com>
2023-06-14 11:16:38 -0400

    updated resume page

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -3,24 +3,24 @@
     <h1>Resume</h1>
 
     <div class="paragraph">
-      I'm <strong>John Matrix</strong>, a former Delta Force operative with lots of experience, lots of training and huge muscles.
-      <br />After graduating from kindergarten, I've been enrolled in the army for 25 years, where I learned to destroy things in an amazing number of ways.
-      <br />I joined the Delta Force because I thought it was Greek lessons, but I quickly adapted to this unexpected environment and became a key element.
-      <br />I love teamwork, but I can also go in solo depending on the mission requirements.
+      I'm <strong>Niles Dorn</strong>, a versatile software developer with experience building games and XR applications. As a recent Pomona College graduate with a degree in physics, I possess strong critical thinking, problem solving, and analytical skills. 
       <br />
-      <br />Right now I'm trying to find inner peace after I've read some personal development books. I'm looking for a monk job so if you have any opportunity feel free to 
+      <br />Working for Post Reality, an augmented reality company, I developed an in-depth knowledge of the software development process from conception to release.
+      <br />
+      <br /> For my senior thesis, I designed and built a VR data visualization tool to model the orbits of asteroids and planets in our solar system.
+      <br />
+      <br />Right now I'm continuing to hone my skills by working on independent projects. I'm looking for a job so if you have any opportunity feel free to 
       <router-link to="/contact">contact me</router-link>.
       <br />
-      <br />Besides war, I'm passionate about firearms, martial arts, explosions, push-ups and flowers.
+      <br />Besides development, I'm passionate about music, gelato, problem solving, and making things.
 
       <div style="margin-top:20px; margin-bottom:20px;">
-        <a class="download-link" href="d/resume-en.pdf" target="_blank"><i class="fa fa-download fa-lg fa-fw"></i> Download as PDF 🇬🇧</a>
-        <a class="download-link" href="d/cv-fr.pdf" target="_blank"><i class="fa fa-download fa-lg fa-fw"></i> Télécharger en PDF 🇫🇷</a>
+        <a class="download-link" href="d/Portfolio_Dorn_Resume.pdf" target="_blank"><i class="fa fa-download fa-lg fa-fw"></i> Download Resume as PDF</a>
         <div style="clear:both"></div>
       </div>
     </div>
     <div class="photo">
-      <img src="img/resume-photo.png" alt="Photo of John" />
+      <img src="img/resume-photo.png" alt="Photo of Niles" />
     </div>
 
     <div style="clear:both"></div>
@@ -28,79 +28,65 @@
     <div class="full-content">
       <h2>Work experience</h2>
 
-      <h3>Operation Classified - Delta Force</h3>
-      <h4>2018-2019</h4>
+      <h3>Head Librarian - Pomona College</h3>
+      <h4>July 2022 - December 2022</h4>
 
-      <div>It's classified so I can't speak about it.</div>
+      <div>Maintained schedules and allocated tasks for a team of 12 student staff members, while adhering to deadlines and reporting to a direct supervisor.</div>
 
-      <div class="tech-stack">Firearms, knives, grenades, Hummer vehicle, parachute,...</div>
+      <div class="tech-stack">Applicable skills: Microsoft Excel, team management, organization, communication, attention to detail.</div>
 
-      <h3>Operation Classified - Delta Force</h3>
-      <h4>2015-2017</h4>
+      <h3>Remote Observer - Pomona College</h3>
+      <h4>February 2019 - August 2022</h4>
 
-      <div>It's classified so I can't speak about it.</div>
+      <div>Worked with the Pomona College-NASA Jet Propulsion Laboratory observing team to collect and analyze observational data on near-Earth objects.</div>
 
-      <div class="tech-stack">Even more Firearms, knives, grenades, Hummer vehicle, parachute,...</div>
+      <div class="tech-stack">Applicable skills: Python, Microsoft Excel, data collection, image analysis, team communication.</div>
 
-      <h3>Operation Classified - Delta Force</h3>
-      <h4>2012-2015</h4>
+      <h3>AR Software Development Intern - Post Reality</h3>
+      <h4>June 2021 - August 2021</h4>
 
-      <div>It's classified so I can't speak about it.</div>
+      <div>Worked with a small team to build Post Reality’s flagship app in Unity from conception to release and provided in-depth quality assurance.</div>
 
-      <div class="tech-stack">Firearms, knives, grenades, Hummer vehicle, parachute,...</div>
+      <div class="tech-stack">Applicable skills: Unity, Xcode, ARKit, C#, Agile.</div>
+
+      <h3>Research Fellow - Pomona College</h3>
+      <h4>May 2020 - August 2020</h4>
+
+      <div>Compiled, organized, and summarized information on an astronomy-specific command line interface by writing a guide for future astronomy students.</div>
+      <div class="tech-stack">Applicable skills: PyRAF, Python, Unix, writing ability, communication.</div>
 
       <h2>Skills</h2>
 
       <div class="skill-set">
-        <h3>Lethal</h3>
-        <SkillRate name="Handguns" :rate="5" />
-        <SkillRate name="Rifles" :rate="5" />
-        <SkillRate name="Shotguns" :rate="5" />
-        <SkillRate name="Machineguns" :rate="5" />
-        <SkillRate name="Missile launchers" :rate="5" />
-        <SkillRate name="Knives" :rate="5" />
-        <SkillRate name="Hand to hand combat" :rate="5" />
+        <h3>Software Development</h3>
+        <SkillRate name="Unity" />
+        <SkillRate name="Unreal" />
+        <SkillRate name="C#" />
+        <SkillRate name="Python" />
+        <SkillRate name="Jira/Agile" />
+        <SkillRate name="Git" />
+        <SkillRate name="Microsoft Office" />
       </div>
 
       <div class="skill-set">
-        <h3>Non-lethal</h3>
-        <SkillRate name="Sneaking" :rate="1" />
-        <SkillRate name="Negotiation" :rate="1" />
-        <SkillRate name="Seduction" :rate="1" />
-        <SkillRate name="Sense of humor" :rate="2" />
+        <h3>Soft Skills</h3>
+        <SkillRate name="Critical Thinking" />
+        <SkillRate name="Problem Solving" />
+        <SkillRate name="Analytical Mindset" />
+        <SkillRate name="Attention to Detail" />
+        <SkillRate name="Communication" />
+        <SkillRate name="Organization" />
+        <SkillRate name="Creativity" />
       </div>
       <div style="clear:both" />
 
       <h2>Education</h2>
 
-      <h3>Kindergarten - 1985</h3>
-      <h4>Kindergarten degree</h4>
-      <div>Specialty in bullying and fighting (58 wins, 58 K-O, 0 losses)</div>
+      <h3>Pomona College - 2022</h3>
+      <h4>B.A., Physics and Astronomy</h4>
+      <div>GPA: 3.42</div>
 
-      <h2>Languages</h2>
-
-      <h3>American</h3>
-      <div>Basic knowledge but I know lots of one-liners</div>
-
-      <h3>That's it</h3>
-      <div>Who needs another language when you speak American?</div>
-
-
-      <h2>Besides...</h2>
-      <div>
-        <h3>I ❤️ Violence</h3>
-        <div>I find it really soothing to just shoot at bad guys</div>
-
-        <h3>I 🧡 Sports</h3>
-        <div>I used to be thin but then I ingested 10 kilos of steroids and proteins a day while doing 1000 push-ups.</div>
-
-        <h3>I 💛 Politics</h3>
-        <div>I hope I can have a career here someday but I might have temper issues.</div>
-
-        <h3>I 💚 Flowers</h3>
-        <div>Especially the big ones, with the trunk and leaves.</div>
-
-      </div>
+      
     </div>
   </div>
 </template>
