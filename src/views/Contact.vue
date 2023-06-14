be3555c71ab30d84ded288cada0435f636b90f2f
ign1ka <nilesdorn@gmail.com>
2023-06-14 12:10:15 -0400

    added itch.io profile link to contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,34 +6,34 @@
     <div style="margin-bottom:40px;">I'm looking for a job!<br/>Feel free to contact me about any opportunity, or just to chat about games, music or anything else.</div>
 
     <ul>
-      <!--<li>
-        <i class="fa fa-user-circle-o fa-lg fa-fw"></i>
-        <a href="http://mywebsite.com" target="_blank">https://mywebsite.com</a>
-      </li>-->
-      <li>
-        <i class="fa fa-envelope-o fa-lg fa-fw"></i>
-        <a href="mailto:nilesdorn@gmail.com" target="_blank">nilesdorn@gmail.com</a>
-      </li>
-      <li>
-        <i class="fa fa-linkedin fa-lg fa-fw" style="vertical-align: -5%;"></i>
-        <a href="https://www.linkedin.com/in/nilesdorn" target="_blank">linkedin.com/in/nilesdorn</a>
-      </li>
-      <li>
-        <i class="fa fa-github fa-lg fa-fw"></i>
-        <a href="https://github.com/nilesdorn" target="_blank">github.com/nilesdorn</a>
-      </li>
-      <!--<li>
-        <i class="fa fa-gamepad fa-lg fa-fw"></i>
-        <a href="https://myself.itch.io" target="_blank">myself.itch.io</a>
-      </li>
-      <li>
-        <i class="fa fa-tumblr fa-lg fa-fw"></i>
-        <a href="https://myblog.tumblr.com" target="_blank">myblog.tumblr.com</a>
-      </li>
-      <li>
-        <i class="fa fa-steam fa-lg fa-fw"></i>
-        <a href="https://steamcommunity.com/id/me" target="_blank">steam.com/me</a>
-      </li>-->
+        <!--<li>
+      <i class="fa fa-user-circle-o fa-lg fa-fw"></i>
+      <a href="http://mywebsite.com" target="_blank">https://mywebsite.com</a>
+    </li>-->
+        <li>
+            <i class="fa fa-envelope-o fa-lg fa-fw"></i>
+            <a href="mailto:nilesdorn@gmail.com" target="_blank">nilesdorn@gmail.com</a>
+        </li>
+        <li>
+            <i class="fa fa-linkedin fa-lg fa-fw" style="vertical-align: -5%;"></i>
+            <a href="https://www.linkedin.com/in/nilesdorn" target="_blank">linkedin.com/in/nilesdorn</a>
+        </li>
+        <li>
+            <i class="fa fa-github fa-lg fa-fw"></i>
+            <a href="https://github.com/nilesdorn" target="_blank">github.com/nilesdorn</a>
+        </li>
+        <li>
+            <i class="fa fa-gamepad fa-lg fa-fw"></i>
+            <a href="https://nilesdorn.itch.io" target="_blank">nilesdorn.itch.io</a>
+        </li>
+        <!--<li>
+      <i class="fa fa-tumblr fa-lg fa-fw"></i>
+      <a href="https://myblog.tumblr.com" target="_blank">myblog.tumblr.com</a>
+    </li>
+    <li>
+      <i class="fa fa-steam fa-lg fa-fw"></i>
+      <a href="https://steamcommunity.com/id/me" target="_blank">steam.com/me</a>
+    </li>-->
     </ul>
 
   </div>
