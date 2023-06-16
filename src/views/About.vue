fcc15cb675b0e3ab3a3664eaa0c3d7caeb4b55e7
ign1ka <85178064+ign1ka@users.noreply.github.com>
2023-06-16 11:00:50 -0400

    text edits

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,7 +6,7 @@
       <div>
         I'm <strong>Niles Dorn</strong>, a versatile software developer and hobbyist musician.<br/>
         
-        I have strong skils across many disciplines, including object-oriented programming, the Unity and Unreal game engines, XR development, and audio production.
+        I have strong skills across many disciplines, including object-oriented programming, the Unity and Unreal game engines, XR development, and audio production.
       </div>
 
       <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">development projects</router-link>, released <router-link to="/other-projects">independent music</router-link>, and applied my abilities in <router-link to="/resume">other fields</router-link> as well.</div>
